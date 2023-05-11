1024ded0a333cae4427a008ea4b7864b6f19c3b3
lfeq <panchocosa@gmail.com>
2023-05-11 13:35:56 -0600

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,13 +8,25 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private float radius = 5f;
 
+    private float angle;
+
+    private void Start()
+    {
+        angle = Random.Range(0f, 360f);
+
+        for (int i = 0; i < 10; i++)
+            SpawnEnemy();
+    }
+
     /// <summary>
     /// Instantiate an enemy in a random position around the player
     /// </summary>
     private void SpawnEnemy()
     {
+        angle = angle * Mathf.Deg2Rad;
+        float angleGap = 5 * Mathf.Deg2Rad;
         Vector3 center = transform.position;
-        float angle = Random.Range(0f, 360f);
+        angle = Random.Range(angle - angleGap, angle + angleGap);
         Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
         Instantiate(enemyPrefab, center + pos, Quaternion.identity);
     }
