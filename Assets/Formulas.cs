5f89cd4bfe4dfc9ebd612f705d76ca1afa151044
lfeq <panchocosa@gmail.com>
2023-05-21 17:33:53 -0600

    Avances rotar objetos alrededor del jugador

--- Assets/Formulas.cs
+++ Assets/Formulas.cs
@@ -51,6 +51,61 @@ public class Formulas {
         return resultado;
     }
 
+    /// <summary>
+    /// Calculates the rotation of a point around a center.
+    /// <example>
+    /// <code>
+    /// <para/>// Rotates in x axis 45 degrees.
+    /// <para/>transform.position = formulas.Quaternion(new Vector3(1, 0, 0), 45, transform.position, center.position);
+    /// <para/>MethodExample(a);
+    /// </code>
+    /// </example>
+    /// </summary>
+    /// <returns>The new position of the rotated object</returns>
+    public Vector3 Quaternion(Vector3 q, float angle, Vector3 pos, Vector3 centro) {
+        Vector3 posicionActualTemp = pos;
+        Vector3 centroTemp = centro;
+        Vector3 distancia = posicionActualTemp - centroTemp;
+
+        //Mover al centro
+        centroTemp = Vector3.zero;
+        posicionActualTemp = Vector3.zero + distancia;
+
+        //Hacer calculos de cuaternion
+        float angleInRad = angle * Mathf.Deg2Rad;
+
+        float w = Mathf.Cos(angleInRad / 2);
+
+        Vector3 v = new Vector3(q.x, q.y, q.z);
+
+        Vector3 v_normal = Normalizar(v);
+
+        v.x = v_normal.x * Mathf.Sin(angleInRad / 2);
+        v.y = v_normal.y * Mathf.Sin(angleInRad / 2);
+        v.z = v_normal.z * Mathf.Sin(angleInRad / 2);
+
+        Matrix4x4 matrix = Matrix4x4.identity;
+
+        matrix.m00 = 1 - 2 * Pow(v.y) - 2 * Pow(v.z);
+        matrix.m01 = 2 * v.x * v.y - 2 * w * v.z;
+        matrix.m02 = 2 * v.x * v.z + 2 * w * v.y;
+
+        matrix.m10 = 2 * v.x * v.y + 2 * w * v.z;
+        matrix.m11 = 1 - 2 * Pow(v.x) - 2 * Pow(v.z);
+        matrix.m12 = 2 * v.y * v.z - 2 * w * v.x;
+
+        matrix.m20 = 2 * v.x * v.z - 2 * w * v.y;
+        matrix.m21 = 2 * v.y * v.z + 2 * w * v.x;
+        matrix.m22 = 1 - 2 * Pow(v.x) - 2 * Pow(v.y);
+
+        //Calcular cuanto se movio
+        Vector3 resultado = matrix.MultiplyPoint(posicionActualTemp);
+        Vector3 movimineto = resultado - posicionActualTemp;
+
+        //Agregar movimiento a la posicion original
+        return pos + movimineto;
+    }
+
     public Vector3 Move(Vector3 pos, Vector3 moveVector) {
         Matrix4x4 matrix = Matrix4x4.identity;
 
